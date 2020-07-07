612c092e66529a96880a8ffea2bfb0e759b53d40
尚雷勋 <leixun.shang@gmail.com>
2020-07-07 09:31:09 +0800

    Initial Commit

---
+++ EightPointMove/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EightPointMove
+//
+//  Created by 尚雷勋 on 2020/7/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
