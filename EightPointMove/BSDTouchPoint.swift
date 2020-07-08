e26323d617f43a4633bdd5c2904e3b1fc848438e
尚雷勋 <leixun.shang@gmail.com>
2020-07-08 16:38:10 +0800

    八点移动，并限制

--- EightPointMove/BSDTouchPoint.swift
+++ EightPointMove/BSDTouchPoint.swift
@@ -13,9 +13,17 @@ class BSDPointManager: NSObject {
         return BSDPointManager()
     }()
     
-    var points = [BSDPoint]()
+    public private(set) var points = [BSDPoint]()
+    
+    public func addPoints(_ points: [BSDPoint]) {
+        for p_ in points {
+            addPoint(p_)
+        }
+    }
     
     /// 添加点
+    ///
+    /// 如果数组中已经有此点，则不添加
     /// - Parameter point: 要添加的点
     public func addPoint(_ point: BSDPoint) {
         
@@ -24,6 +32,14 @@ class BSDPointManager: NSObject {
         }
         
         points.append(point)
+    }
+    
+    public func setUpAllPoints() {
+        
+        for (idx, var _p) in points.enumerated() {
+            _p.addBondPoints(points)
+            points[idx] = _p
+        }
     }
     
     /// 移除点
@@ -40,32 +56,112 @@ class BSDPointManager: NSObject {
     /// 更新点
     /// - Note: 更新点的时候，必须是已经添加了所有需要用到的点
     /// - Parameter point: 要更新的点
-    func updatePoint(_ point: BSDPoint) {
-        for var point_ in points {
-            point_.updateBondPoint(point: point)
+    private func updatePoint(_ point: BSDPoint) {
+        
+        guard let idx = points.firstIndex(of: point) else {
+            return
         }
+        points[idx] = point
+        
+        for (idx, var point_) in points.enumerated() {
+            point_.updateBondPoint(point)
+            points[idx] = point_
+        }
+        
     }
     
+    public func limitPoint(_ newPoint: BSDPoint) -> BSDPoint? {
+        
+        if var nP = pointFromLevel(newPoint.level) {
+            nP.limitPoint(newPoint.point)
+            updatePoint(nP)
+            // 返回更新之后的点
+            return pointFromLevel(nP.level)
+        }
+        
+        return nil
+    }
+    
+    private func pointFromLevel(_ level: CGPoint) -> BSDPoint? {
+        
+        for point in points {
+            if point.level == level {
+                return point
+            }
+        }
+        return nil
+    }
     
 }
 
-struct BSDPoint: Equatable {
+struct BSDSideColor {
+    
+    private enum Side: String {
+        case top
+        case left
+        case right
+        case bottom
+    }
+    
+    private var colors = [Side : UIColor]()
+    
+    public var top: UIColor? {
+        colors[.top] ?? nil
+    }
+    
+    public var left: UIColor? {
+        colors[.left] ?? nil
+    }
+    
+    public var right: UIColor? {
+        colors[.right] ?? nil
+    }
+    
+    public var bottom: UIColor? {
+        colors[.bottom] ?? nil
+    }
+    
+    public static func make(_ top: UIColor?, _ left: UIColor?, _ right: UIColor?, _ bottom: UIColor?) -> Self {
+        Self.init(top: top, left: left, right: right, bottom: bottom)
+    }
+    
+    public static var empty: BSDSideColor {
+        return Self.init(top: nil, left: nil, right: nil, bottom: nil)
+    }
+    
+    public init(top: UIColor?, left: UIColor?, right: UIColor?, bottom: UIColor?) {
+        
+        if let top = top {
+            colors[.top] = top
+        }
+        
+        if let left = left {
+            colors[.left] = left
+        }
+        
+        if let right = right {
+            colors[.right] = right
+        }
+        
+        if let bottom = bottom {
+            colors[.bottom] = bottom
+        }
+        
+    }
+}
+
+
+struct BSDPoint {
     
     /// 构造函数
     /// - Parameters:
     ///   - horizontalLevel: 水平方向的层级，从 0 开始
     ///   - verticalLevel: 垂直方向的层级，从 0 开始
     ///   - point: 一般来说，这里应该是像素点
-    init(horizontalLevel: Int, verticalLevel: Int, point: CGPoint) {
+    public init(horizontalLevel: Int, verticalLevel: Int, point: CGPoint, sideColor: BSDSideColor = BSDSideColor.empty) {
         level = CGPoint(x: horizontalLevel, y: verticalLevel)
         self.point = point
-    }
-    
-    static func == (lhs: Self, rhs: Self) -> Bool {
-        if lhs.level == rhs.level {
-            return true
-        }
-        return false
+        self.sideColor = sideColor
     }
     
     /// 关联点方位
@@ -73,67 +169,94 @@ struct BSDPoint: Equatable {
         /// 与此点无关系的点
         case none
         /// 在此点上方
-        case up
+        case top
         /// 在此点左方
         case left
         /// 在此点右方
         case right
         /// 在此点下方
-        case down
+        case bottom
     }
-
+    
     public var level: CGPoint = CGPoint.zero
     public var point: CGPoint = CGPoint.zero
+    private var sideColor: BSDSideColor
+    
     public var bondPoints = [BondPointPosition : BSDPoint]()
-    public var bondColors = [BondPointPosition : UIColor]()
+    public var bondColors: [BondPointPosition : UIColor?] {
+        [ .top: sideColor.top, .left: sideColor.left, .right: sideColor.right, .bottom: sideColor.bottom]
+    }
+    
+    public static var zero: BSDPoint {
+        return BSDPoint(horizontalLevel: 0, verticalLevel: 0, point: CGPoint.zero, sideColor: BSDSideColor.empty)
+    }
+    
+    /// 添加多个关联点
+    ///
+    /// 会自动筛选，如果不是和本点有关联的点，则不添加
+    ///
+    /// - Parameter points:
+    public mutating func addBondPoints(_ points: [BSDPoint]) {
+        for p_ in points {
+            addBondPoint(p_)
+        }
+    }
     
     /// 添加关联点
+    ///
+    /// 会自动筛选，如果不是和本点有关联的点，则不添加
     /// - Parameters:
     ///   - point: 点
-    ///   - color: 与本点的连线的颜色
-    mutating func addBondPoint(point: BSDPoint, color: UIColor) {
+    public mutating func addBondPoint(_ point: BSDPoint) {
         
-        guard pointToPosition(point) != .none else {
+        guard positionFromPoint(point) != .none else {
             return
         }
-        let pos = pointToPosition(point)
+        let pos = positionFromPoint(point)
         
         bondPoints[pos] = point
-        bondColors[pos] = color
     }
     
     /// 更新关联点
     /// - Parameter point: 与本点关联的点
-    mutating func updateBondPoint(point: BSDPoint) {
+    public mutating func updateBondPoint(_ point: BSDPoint) {
         
-        guard pointToPosition(point) != .none else {
+        guard positionFromPoint(point) != .none else {
             return
         }
         
-        bondPoints[ pointToPosition(point) ] = point
+        bondPoints[ positionFromPoint(point) ] = point
     }
     
     /// 由点的 level 推断出与本点的方位关系
     /// - Parameter point: 需要推断的点
     /// - Returns: 与本点的方位关系
-    private func pointToPosition(_ point: BSDPoint) -> BondPointPosition {
+    private func positionFromPoint(_ point: BSDPoint) -> BondPointPosition {
         
         let inLevel = point.level
         let meLevel = self.level
         if inLevel.x == meLevel.x && inLevel.y == meLevel.y + 1 {
-            return .up
+            return .top
         }
         
-        if inLevel.y == meLevel.y && inLevel.x == meLevel.x - 1 {
+        else if inLevel.y == meLevel.y && inLevel.x == meLevel.x - 1 {
+            // 横轴最小点为 0，不能小于它
+            if meLevel.x == 0 {
+                return .none
+            }
             return .left
         }
         
-        if inLevel.y == meLevel.y && inLevel.x == meLevel.x + 1 {
+        else if inLevel.y == meLevel.y && inLevel.x == meLevel.x + 1 {
             return .right
         }
         
-        if inLevel.x == meLevel.x && inLevel.y == meLevel.y - 1 {
-            return .down
+        else if inLevel.x == meLevel.x && inLevel.y == meLevel.y - 1 {
+            // 纵轴最小点为 0，不能小于此点
+            if meLevel.y == 0 {
+                return .none
+            }
+            return .bottom
         }
         
         return .none
@@ -143,14 +266,14 @@ struct BSDPoint: Equatable {
     /// - Parameter newPoint: 可能要移动到的新点
     /// - Returns: 限制后的只能到达的点
     @discardableResult
-    public mutating func limitPoint(newPoint: CGPoint) -> CGPoint {
+    public mutating func limitPoint(_ newPoint: CGPoint) -> CGPoint {
         guard newPoint != self.point else {
             return self.point
         }
         
         var outPoint = newPoint
         
-        if let upPoint = bondPoints[.up] {
+        if let upPoint = bondPoints[.top] {
             if newPoint.y > upPoint.point.y {
                 outPoint.y = upPoint.point.y
             }
@@ -168,7 +291,7 @@ struct BSDPoint: Equatable {
             }
         }
         
-        if let downPoint = bondPoints[.down] {
+        if let downPoint = bondPoints[.bottom] {
             if newPoint.y < downPoint.point.y {
                 outPoint.y = downPoint.point.y
             }
@@ -178,19 +301,87 @@ struct BSDPoint: Equatable {
         
         return outPoint
     }
-    
-    
+}
 
+extension BSDPoint: Equatable {
+    
+    public static func == (lhs: Self, rhs: Self) -> Bool {
+        if lhs.level == rhs.level {
+            return true
+        }
+        return false
+    }
 }
 
 class BSDTouchPoint: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    public var point: BSDPoint = BSDPoint.zero
+    public var panBlock: ((UIGestureRecognizer.State, CGPoint) -> Void)?
+    
+    public init(frame: CGRect, point: BSDPoint) {
+        super.init(frame: frame)
+        self.point = point
+        // addLongPressGesture()
+        addPanGesture()
+        
+        backgroundColor = UIColor.black.withAlphaComponent(0.4)
+        layer.cornerRadius = frame.width/2.0
+        layer.masksToBounds = true
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addLongPressGesture() {
+        let longPress = UILongPressGestureRecognizer(target: self, action: #selector(longPressEvent(_:)))
+        self.addGestureRecognizer(longPress)
+    }
+    
+    private func addPanGesture() {
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(panEvent(_:)))
+        self.addGestureRecognizer(pan)
+    }
+    
+    @objc private func longPressEvent(_ sender: UILongPressGestureRecognizer) {
+        guard sender.view != nil else {
+            return
+        }
+        panBlock?(sender.state, sender.location(in: self))
+    }
+    
+    @objc private func panEvent(_ sender: UIPanGestureRecognizer) {
+        guard sender.view != nil else {
+            return
+        }
+        
+        if sender.state == .began {
+            backgroundColor = UIColor.cyan.withAlphaComponent(0.4)
+        }
+        if sender.state == .ended || sender.state == .cancelled {
+            backgroundColor = UIColor.black.withAlphaComponent(0.4)
+        }
+        
+        if sender.state == .changed {
+            
+            point.point = sender.location(in: self.superview)
+            
+            if let limitPoint = BSDPointManager.default.limitPoint(point) {
+                // 更新 touch 的 point
+                self.point = limitPoint
+                panBlock?(sender.state, limitPoint.point)
+            }
+            
+        }
+    }
+    
+    
+    /*
+     // Only override draw() if you perform custom drawing.
+     // An empty implementation adversely affects performance during animation.
+     override func draw(_ rect: CGRect) {
+     // Drawing code
+     }
+     */
+    
 }
--- EightPointMove/ViewController.swift
+++ EightPointMove/ViewController.swift
@@ -7,11 +7,110 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+extension CGPoint {
+    
+    public static func make(_ x: Int, _ y: Int) -> CGPoint {
+        CGPoint(x: x, y: y)
+    }
+    
+    public static func make(_ x: CGFloat, _ y: CGFloat) -> CGPoint {
+        CGPoint(x: x, y: y)
+    }
+    
+    public static func make(_ x: Double, _ y: Double) -> CGPoint {
+        CGPoint(x: x, y: y)
+    }
+}
 
+class ViewController: UIViewController {
+    
+    var bsdTouch: BSDTouchPoint?
+    
+    var bsdTouches = [BSDTouchPoint]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
+        var sizeArray: [CGSize] = [ .init(width: 7, height: 8), .init(width: 20, height: 20) ]
+        
+        print("before ", sizeArray)
+        for (idx, var lSize) in sizeArray.enumerated() {
+            lSize.width += 5
+            sizeArray[idx] = lSize
+        }
+        
+        print("after", sizeArray)
+        
+        
+        let sideColors: [BSDSideColor] = [ .make(nil, nil, .red, .blue),
+                                           .make(.blue, nil, .red, nil),
+                                           .make(nil, .red, .yellow, .red),
+                                           .make(.red, .red, .yellow, nil),
+                                           .make(nil, .yellow, .orange, .yellow),
+                                           .make(.yellow, .yellow, .orange, nil),
+                                           .make(nil, .orange, nil, .orange),
+                                           .make(.orange, .orange, nil, nil) ]
+        
+        let levels: [CGPoint] = [ .make(0, 0), .make(0, 1),
+                                  .make(1, 0), .make(1, 1),
+                                  .make(2, 0), .make(2, 1),
+                                  .make(3, 0), .make(3, 1)]
+        
+        let oriCoordi: [CGPoint] = [ .make(334/2.0, 174/2.0), .make(322/2.0, 682/2.0),
+                                     .make(568/2.0, 168/2.0), .make(692/2.0, 682/2.0),
+                                     .make(850/2.0, 166/2.0), .make(976/2.0, 682/2.0),
+                                     .make(1172/2.0, 162/2.0), .make(1260/2.0, 682/2.0)]
+        
+        var points = [BSDPoint]()
+         
+        let size = CGSize(width: 50, height: 50)
+        
+        for idx in 0..<8 {
+            
+            let ori = oriCoordi[idx]
+            
+            let bsdP = BSDPoint(horizontalLevel: Int(levels[idx].x) , verticalLevel: Int(levels[idx].y), point: oriCoordi[idx], sideColor: sideColors[idx])
+            points.append(bsdP)
+            
+            let bsdTP = BSDTouchPoint(frame: CGRect(origin: ori, size: size), point: bsdP)
+            
+            bsdTP.panBlock = { (_, viewPoint) in
+                
+                UIView.animate(withDuration: 0.1) {
+                    bsdTP.center = viewPoint
+                }
+            }
+            
+            bsdTP.center = bsdTP.frame.origin
+            
+            view.addSubview(bsdTP)
+            
+            bsdTouches.append(bsdTP)
+        }
+        
+        
+        BSDPointManager.default.addPoints(points)
+        BSDPointManager.default.setUpAllPoints()
+        
+        
+//        bsdTouch = BSDTouchPoint(frame: CGRect(x: 50, y: 50, width: 60, height: 60), point: BSDPoint(horizontalLevel: 0, verticalLevel: 0, point: CGPoint.zero, sideColor: BSDSideColor.empty))
+//        
+//        bsdTouch?.panBlock = { [weak self] (state, viewPoint) in
+//            
+//            guard let self = self else {
+//                return
+//            }
+//            
+//            UIView.animate(withDuration: 0.1) {
+//                self.bsdTouch?.center = viewPoint
+//            }
+//        }
+//        
+//        view.addSubview(bsdTouch!)
+        
+        
     }
 
 
